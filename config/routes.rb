863ab035ce3611fd4fd8da551a808a4d90ae0fd0
Jaime <mrjaime1999@gmail.com>
2020-02-19 09:52:50 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  #get 'principal/index'
+
+
+
+  resources :canciones do
+    resources :parrafos do
+      resources :comentarios
+    end
+  end
+
+  resources :insultos
+  resources :anuncios
+
+  root 'principal#index'
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
