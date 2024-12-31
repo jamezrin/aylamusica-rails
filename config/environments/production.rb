d2c4fe45c346124c7e94329687a8d8a9d4d7d2bc
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-12-31 16:03:12 +0100

    fix: domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "aylamusica.jamezrin.name"
+  config.hosts << "aylamusica.jamezrin.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
